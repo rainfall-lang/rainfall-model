98f9290aa11575528a6857979d7c63e3781905ac
Amos Robinson <amos.robinson@gmail.com>
2019-04-22 16:39:27 +1000

    proof: show example evaluation for coin scenario

--- proof/Examples.thy
+++ proof/Examples.thy
@@ -4,59 +4,62 @@ theory Examples
 begin
 
 section \<open>Coin transfer rule\<close>
+(* This is the 'transfer' example from the paper.
+   The rule is based on the desugared version from Figure 4 *)
 
 definition "transfer_rn = rule_name ''transfer''"
 
 (* Accept facts: constructor and accessor functions *)
-definition mk_AcceptPayload where
+(* Put everything in the simpset using [simp], because we don't want to have to manually unfold it later *)
+definition mk_AcceptPayload where [simp]:
   "mk_AcceptPayload oid accepter = vpair (vlit (lnat oid)) (vlit (lparty accepter))"
 
 fun tk_AcceptPayload where
   "tk_AcceptPayload (vpair (vlit (lnat oid)) (vlit (lparty accepter))) = (oid, accepter)"
 | "tk_AcceptPayload _ = undefined"
 
-definition "Accept_oid      = fst o tk_AcceptPayload o fact_value"
-definition "Accept_accepter = snd o tk_AcceptPayload o fact_value"
+definition [simp]: "Accept_oid      = fst o tk_AcceptPayload o fact_value"
+definition [simp]: "Accept_accepter = snd o tk_AcceptPayload o fact_value"
 
-definition "Accept = fact_ctor ''Accept''"
-definition "mk_Accept oid accepter by obs = \<lparr>fact_name = Accept, fact_value = mk_AcceptPayload oid accepter, fact_by = by, fact_obs = obs, fact_rules = {transfer_rn}\<rparr>"
+definition [simp]: "Accept = fact_ctor ''Accept''"
+definition [simp]: "mk_Accept oid accepter by obs = \<lparr>fact_name = Accept, fact_value = mk_AcceptPayload oid accepter, fact_by = by, fact_obs = obs, fact_rules = {transfer_rn}\<rparr>"
 
 (* Offer facts *)
-definition mk_OfferPayload where
+definition mk_OfferPayload where [simp]:
   "mk_OfferPayload oid terms giver receiver = vpair (vlit (lnat oid)) (vpair (vlit (lsymbol terms)) (vpair (vlit (lparty giver)) (vlit (lparty receiver))))"
 
 fun tk_OfferPayload where
   "tk_OfferPayload (vpair (vlit (lnat oid)) (vpair (vlit (lsymbol terms)) (vpair (vlit (lparty giver)) (vlit (lparty receiver))))) = (oid, terms, giver, receiver)"
 | "tk_OfferPayload _ = undefined"
 
-definition "Offer_oid      =             fst o tk_OfferPayload o fact_value"
-definition "Offer_terms    =       fst o snd o tk_OfferPayload o fact_value"
-definition "Offer_giver    = fst o snd o snd o tk_OfferPayload o fact_value"
-definition "Offer_receiver = snd o snd o snd o tk_OfferPayload o fact_value"
+definition [simp]: "Offer_oid      =             fst o tk_OfferPayload o fact_value"
+definition [simp]: "Offer_terms    =       fst o snd o tk_OfferPayload o fact_value"
+definition [simp]: "Offer_giver    = fst o snd o snd o tk_OfferPayload o fact_value"
+definition [simp]: "Offer_receiver = snd o snd o snd o tk_OfferPayload o fact_value"
 
-definition "Offer  = fact_ctor ''Offer''"
-definition "mk_Offer oid terms giver receiver by obs = \<lparr>fact_name = Offer, fact_value = mk_OfferPayload oid terms giver receiver, fact_by = by, fact_obs = obs, fact_rules = {transfer_rn}\<rparr>"
+definition [simp]: "Offer  = fact_ctor ''Offer''"
+definition [simp]: "mk_Offer oid terms giver receiver by obs = \<lparr>fact_name = Offer, fact_value = mk_OfferPayload oid terms giver receiver, fact_by = by, fact_obs = obs, fact_rules = {transfer_rn}\<rparr>"
 
 (* Coin facts *)
-definition mk_CoinPayload where
+definition mk_CoinPayload where [simp]:
   "mk_CoinPayload issuer holder = vpair (vlit (lparty issuer)) (vlit (lparty holder))"
 
 fun tk_CoinPayload where
   "tk_CoinPayload (vpair (vlit (lparty issuer)) (vlit (lparty holder))) = (issuer, holder)"
 | "tk_CoinPayload _ = undefined"
 
-definition "Coin_issuer = fst o tk_CoinPayload o fact_value"
-definition "Coin_holder = snd o tk_CoinPayload o fact_value"
+definition [simp]: "Coin_issuer = fst o tk_CoinPayload o fact_value"
+definition [simp]: "Coin_holder = snd o tk_CoinPayload o fact_value"
 
-definition "Coin = fact_ctor ''Coin''"
-definition "mk_Coin issuer holder by obs = \<lparr>fact_name = Coin, fact_value = mk_CoinPayload issuer holder, fact_by = by, fact_obs = obs, fact_rules = {transfer_rn}\<rparr>"
+definition [simp]: "Coin = fact_ctor ''Coin''"
+definition [simp]: "mk_Coin issuer holder by obs = \<lparr>fact_name = Coin, fact_value = mk_CoinPayload issuer holder, fact_by = by, fact_obs = obs, fact_rules = {transfer_rn}\<rparr>"
 
 (* Transfer rule *)
-definition "accept_v = fact_var ''accept''"
-definition "offer_v  = fact_var ''offer''"
-definition "coin_v   = fact_var ''coin''"
+definition [simp]: "accept_v = fact_var ''accept''"
+definition [simp]: "offer_v  = fact_var ''offer''"
+definition [simp]: "coin_v   = fact_var ''coin''"
 
-definition
+definition [simp]:
   "match_Accept = match
     accept_v
     (gather_when Accept (\<lambda>h. True))
@@ -64,7 +67,7 @@ definition
     (consume_weight (\<lambda>h. 1))
     (gain_auth (\<lambda>h. {Accept_accepter (h accept_v)}))"
 
-definition
+definition [simp]:
   "match_Offer = match
     offer_v
     (gather_when Offer
@@ -74,7 +77,7 @@ definition
     (consume_weight (\<lambda>h. 1))
     (gain_auth (\<lambda>h. {Offer_giver (h offer_v)}))"
 
-definition
+definition [simp]:
   "match_Coin = match
     coin_v
     (gather_when Coin (\<lambda>h. Coin_holder (h coin_v) = Offer_giver (h offer_v)))
@@ -82,7 +85,7 @@ definition
     (consume_weight (\<lambda>h. 1))
     (gain_auth (\<lambda>h. {Coin_issuer (h coin_v)}))"
 
-definition
+definition [simp]:
   "transfer_Say =
     (\<lambda>h. {# mk_Coin
               (Coin_issuer    (h coin_v))
@@ -91,36 +94,49 @@ definition
               (fact_obs (h coin_v))
          #})"
 
-definition
+definition [simp]:
   "transfer = rule transfer_rn [match_Accept, match_Offer, match_Coin] transfer_Say"
 
 subsection \<open>Coin transfer scenario\<close>
 
-definition "Alice    = party ''Alice''"
-definition "Bob      = party ''Bob''"
-definition "Mona     = party ''Mona''"
-definition "Isabelle = party ''Isabelle''"
-definition "oid      = (1234 :: nat)"
-definition "terms    = symbol ''To purchase a guitar''"
+(* The guitar purchase example from S2.1 *)
+definition [simp]: "Alice    = party ''Alice''"
+definition [simp]: "Bob      = party ''Bob''"
+definition [simp]: "Mona     = party ''Mona''"
+definition [simp]: "Isabelle = party ''Isabelle''"
+definition [simp]: "oid      = (1234 :: nat)"
+definition [simp]: "terms    = symbol ''To purchase a guitar''"
 
-definition "store0 = {#
-    mk_Offer  oid terms Alice Bob {Bob}             {Mona, Alice},
-    mk_Accept oid       Bob       {Alice}           {Mona, Bob},
+definition [simp]:
+ "store0 = {#
+    mk_Offer  oid terms Alice Bob {Alice}           {Mona, Bob},
+    mk_Accept oid       Bob       {Bob}             {Mona, Alice},
     mk_Coin   Isabelle  Alice     {Isabelle, Alice} {Mona}
   #}"
 
-definition "store1 = {#
+definition [simp]:
+ "store1 = {#
     mk_Coin   Isabelle  Bob       {Isabelle, Bob} {Mona}
   #}"
 
-(*
-lemma "{Alice} | store0 \<turnstile> transfer \<Down> {#} | store1 | store0 | store1 FIRE"
-  unfolding transfer_def match_Accept_def match_Offer_def match_Coin_def accept_v_def offer_v_def coin_v_def store0_def store1_def transfer_Say_def
-  apply (intro EvFire.intros EvExec.intros; simp?)
+(* If we have an offer, acceptance and a coin, we can transfer the coin to the seller. *)
+lemma "{Alice} | store0 \<turnstile> transfer \<Down> store0 | store0 | store1 | store1 FIRE"
   apply simp
-  apply (intro EvMatch.intros)
-  sledgehammer
-*)
+  apply (intro EvFire.intros; simp?)
+    apply (rule EvMatches.intros; simp?)
+       apply (intro EvMatch.intros EvGather.intros EvConsume.intros EvSelect.intros EvGain.intros; (simp add: check_gather_def can_see_fact_def)?; simp?; simp?)
+      apply (rule EvMatches.intros; simp?)
+       apply (intro EvMatch.intros EvGather.intros EvConsume.intros EvSelect.intros EvGain.intros; (simp add: check_gather_def can_see_fact_def)?; simp?; simp?)
+      apply (rule EvMatches.intros; simp?)
+       apply (intro EvMatch.intros EvGather.intros EvConsume.intros EvSelect.intros EvGain.intros; (simp add: check_gather_def can_see_fact_def)?; simp?; simp?)
+      apply (rule EvMatches.intros; simp?)
+     apply simp
+    apply simp
+   apply (intro EvExec.intros)
+   apply simp
+  apply simp
+  done
+
 
 section \<open>Test find_firsts\<close>
 (* A small test to make sure the 'minimum' in find_firsts works as expected *)
