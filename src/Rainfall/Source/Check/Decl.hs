84c256f6ff5f3aa81fe74436330e836b0212cd4a
Ben Lippmeier <benl@ouroborus.net>
2019-05-11 16:09:17 +1000

    source: check select/consume/gain clauses

--- src/Rainfall/Source/Check/Decl.hs
+++ src/Rainfall/Source/Check/Decl.hs
@@ -21,7 +21,8 @@ checkDecls ds
 checkDecl :: Facts RL -> Decl RL -> IO (Decl RL)
 checkDecl facts dd
  = case dd of
-        DeclFact{}       -> return dd
+        DeclFact{}
+         -> return dd
 
         DeclRule a rule
          -> DeclRule a     <$> checkRule a facts rule
@@ -54,6 +55,7 @@ checkRule a facts (Rule nRule hsMatch mBody)
 checkMatches
         :: RL -> Context RL
         -> [Match RL] -> IO (Context RL, [Match RL])
+
 checkMatches _a ctx []
  = return (ctx, [])
 
@@ -75,11 +77,10 @@ checkMatch _a ctx (MatchAnn a h)
 checkMatch a ctx (Match gather select consume gain)
  = do
         (ctx', gather') <- checkGather a ctx gather
-        -- TODO: check select
-        -- TODO: check consume
-        -- TODO: check gain
-
-        return (ctx', Match gather' select consume gain)
+        select'  <- checkSelect  a ctx' select
+        consume' <- checkConsume a ctx' consume
+        gain'    <- checkGain    a ctx' gain
+        return (ctx', Match gather' select' consume' gain')
 
 
 ----------------------------------------------------------------------------------------- Gather --
@@ -110,6 +111,7 @@ checkGather a ctx (GatherPat nFact ngps mmPred)
                 Nothing -> return Nothing
                 Just mPred
                  -> do  (mPred', tPred) <- checkTerm a ctx' mPred
+
                         when (not $ checkEq tPred TBool) $ nope a
                          $ [ text "predicate expression"
                            , text " of type " <> squotes (ppType tPred)
@@ -184,6 +186,58 @@ checkGatherPat a nField tField ctx (GatherPatEq mMatch)
            , text " does not field type " <> squotes (ppType tField) ]
 
         return (ctx, GatherPatEq mMatch')
+
+
+----------------------------------------------------------------------------------------- Select --
+-- | Check a select clause.
+checkSelect :: RL -> Context RL -> Select RL -> IO (Select RL)
+checkSelect _a ctx (SelectAnn a select)
+ = do   select' <- checkSelect a ctx select
+        return $ SelectAnn a select'
+
+checkSelect _a _ctx s@SelectAny
+ = do   return s
+
+checkSelect a ctx (SelectFirst mKey)
+ = do   mKey'   <- checkTermIs a TNat ctx mKey
+        return $ SelectFirst mKey'
+
+checkSelect a ctx (SelectLast mKey)
+ = do   mKey'   <- checkTermIs a TNat ctx mKey
+        return $ SelectLast mKey'
+
+
+---------------------------------------------------------------------------------------- Consume --
+-- | Check a consume clause.
+checkConsume :: RL -> Context RL -> Consume RL -> IO (Consume RL)
+checkConsume _a ctx (ConsumeAnn a consume)
+ = do   consume' <- checkConsume a ctx consume
+        return $ ConsumeAnn a consume'
+
+checkConsume _a _ctx c@ConsumeNone
+ = do   return c
+
+checkConsume a ctx (ConsumeWeight mWeight)
+ = do   mWeight' <- checkTermIs a TNat ctx mWeight
+        return  $ ConsumeWeight mWeight'
+
+
+------------------------------------------------------------------------------------------- Gain --
+checkGain :: RL -> Context RL -> Gain RL -> IO (Gain RL)
+checkGain _a ctx (GainAnn a gain)
+ = do   gain' <- checkGain a ctx gain
+        return $ GainAnn a gain'
+
+checkGain _a _ctx gg@GainNone
+ = do   return gg
+
+checkGain a ctx (GainCheck mParties)
+ = do   mParties' <- checkTermIs a (TSet TParty) ctx mParties
+        return $ GainCheck mParties'
+
+checkGain a ctx (GainTerm mParties)
+ = do   mParties' <- checkTermIs a (TSet TParty) ctx mParties
+        return $ GainTerm mParties'
 
 
 --------------------------------------------------------------------------------------- Scenario --
